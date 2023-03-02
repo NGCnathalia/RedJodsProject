9f8c7e3c6e8ac1136b0c2989779699dd802cfc90
ruthyi <jbruthlizcano@gmail.com>
2023-03-01 22:33:53 -0500

    add

--- config/mongo.js
+++ config/mongo.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const dbConnect =() =>{
     const MONGO_URI= process.env.MONGO_URI;
-    console.log(MONGO_URI);
     mongoose.set("strictQuery", false);
     mongoose.connect(MONGO_URI,{
       useNewUrlParser:true,
