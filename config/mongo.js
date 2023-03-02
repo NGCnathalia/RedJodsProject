da94f911347be4df7633e3dea349a02a8ef52860
ruthyi <jbruthlizcano@gmail.com>
2023-03-01 22:27:16 -0500

    conección a la base de datos

---
+++ config/mongo.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const dbConnect =() =>{
+    const MONGO_URI= process.env.MONGO_URI;
+    console.log(MONGO_URI);
+    mongoose.set("strictQuery", false);
+    mongoose.connect(MONGO_URI,{
+      useNewUrlParser:true,
+      useUnifiedTopology:true
+    },(err, res)=>{
+      if(!err){
+        console.log("*** Conexion Correcta ***")
+      }else{
+        console.log("*** Error de Conexion ***")
+      }
+    })
+}
+module.exports={
+  dbConnect
+}
+
